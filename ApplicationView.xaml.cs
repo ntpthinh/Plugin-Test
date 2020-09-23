f84a6fd2d7789715a4fdb7a01c9d94f281214b4a
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2020-09-23 15:04:11 +0700

    First commit

---
+++ ApplicationView.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+namespace ADNMenuSample
+{
+    /// <summary>
+    /// Interaction logic for ApplicationView.xaml
+    /// </summary>
+    public partial class ApplicationView : Window
+    {
+        public ApplicationView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string modelLocation = @"D:\model.onnx";
+                /*MLContext mlContext = new MLContext();
+                                string modelLocation = "/ADNMenuSample;Component/model.zip";
+
+                Uri uri = new Uri(modelLocation, UriKind.Relative);
+                System.Windows.Resources.StreamResourceInfo info = Application.GetResourceStream(uri);
+                ITransformer trainedModel = mlContext.Model.Load(info.Stream, out DataViewSchema modelSchema);*/
+                MLContext mlContext = new MLContext();
+                var data = mlContext.Data.LoadFromEnumerable(new List<ImageData>());
+                var pipeline = mlContext.Transforms.LoadImages(outputColumnName: "image", imageFolder: "", inputColumnName: nameof(ImageData.ImagePath))
+                        .Append(mlContext.Transforms.ResizeImages(outputColumnName: ModelSettings.ModelInput, imageWidth: ImageResNetSettings.imageWidth, imageHeight: ImageResNetSettings.imageHeight, inputColumnName: "image"))
+                        .Append(mlContext.Transforms.ExtractPixels(outputColumnName: ModelSettings.ModelInput))
+                        .Append(mlContext.Transforms.CustomMapping(new NormalizeMapping().GetMapping(), contractName: nameof(NormalizeMapping)))
+                        .Append(mlContext.Transforms.ApplyOnnxModel(modelFile: modelLocation, outputColumnNames: new[] { ModelSettings.ModelOutput }, inputColumnNames: new[] { ModelSettings.ModelInput }));
+                var model = pipeline.Fit(data);
+                mlContext.ComponentCatalog.RegisterAssembly(typeof(NormalizeMapping).Assembly);
+                IEnumerable<ImageData> image = ImageData.ReadImageFromPaths(new string[] { @"E:\test.jpg" });
+                IDataView imageDataView = mlContext.Data.LoadFromEnumerable(image);
+                IDataView scoredData = model.Transform(imageDataView);
+                List<float[]> probabilities = scoredData.GetColumn<float[]>(ModelSettings.ModelOutput).ToList();
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        public class ImageData
+        {
+            [Microsoft.ML.Data.LoadColumn(0)]
+            public string ImagePath;
+
+            [Microsoft.ML.Data.LoadColumn(1)]
+            public string Label;
+            public ImageData()
+            {
+
+            }
+            public ImageData(string imagePath)
+            {
+                ImagePath = imagePath;
+            }
+            public static IEnumerable<ImageData> ReadFromFile(string imageFolder)
+            {
+                return Directory
+                    .GetFiles(imageFolder)
+                    .Where(filePath => System.IO.Path.GetExtension(filePath) != ".md")
+                    .Select(filePath => new ImageData { ImagePath = filePath, Label = System.IO.Path.GetFileName(filePath) });
+            }
+            public static IEnumerable<ImageData> ReadImageFromPaths(string[] imagePaths)
+            {
+                return imagePaths.Select(filePath => new ImageData { ImagePath = filePath, Label = System.IO.Path.GetFileName(filePath) });
+            }
+        }
+        public struct ImageResNetSettings
+        {
+            public const int imageHeight = 224;
+            public const int imageWidth = 224;
+            public const float mean = 117;             // (offsetImage: ImageSettingsForTFModel.mean)
+            public const float scale = 1 / 255f;         //1/255f for InceptionV3. Not used for InceptionV1
+            public const bool channelsLast = true;
+
+        }
+        public struct ModelSettings
+        {
+
+            // input tensor name
+            public const string ModelInput = "input";
+
+            // output tensor name
+            public const string ModelOutput = "output";
+        }
+        [Microsoft.ML.Transforms.CustomMappingFactoryAttribute(nameof(NormalizeMapping))]
+        public class NormalizeMapping : Microsoft.ML.Transforms.CustomMappingFactory<NormalizeInput, NormalizeOutput>
+        {
+            // This is the custom mapping. We now separate it into a method, so that we can use it both in training and in loading.
+            public static void Mapping(NormalizeInput input, NormalizeOutput output)
+            {
+                var values = input.Reshape.GetValues().ToArray();
+
+                var image_mean = new float[] { 0.5f, 0.5f, 0.5f };
+                var image_std = new float[] { 0.5f, 0.5f, 0.5f };
+
+                for (int x = 0; x < values.Count(); x++)
+                {
+                    var y = x % 3;
+                    // Normalize by 255 first
+                    values[x] /= 255;
+                    values[x] = (values[x] - image_mean[y]) / image_std[y];
+                };
+
+                output.Reshape = new Microsoft.ML.Data.VBuffer<float>(values.Count(), values);
+            }
+            // This factory method will be called when loading the model to get the mapping operation.
+            public override Action<NormalizeInput, NormalizeOutput> GetMapping()
+            {
+                return Mapping;
+            }
+        }
+        public class NormalizeInput
+        {
+            [Microsoft.ML.Data.ColumnName("input")]
+            [VectorType(3, 224, 224)]
+            public VBuffer<float> Reshape;
+        }
+        public class NormalizeOutput
+        {
+            [ColumnName("output")]
+            [VectorType(3 * 224 * 224)]
+            public VBuffer<float> Reshape;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿#region Copyright
+//      .NET Sample
+//
+//      Copyright (c) 2012 by Autodesk, Inc.
+//
+//      Permission to use, copy, modify, and distribute this software
+//      for any purpose and without fee is hereby granted, provided
+//      that the above copyright notice appears in all copies and
+//      that both that copyright notice and the limited warranty and
+//      restricted rights notice below appear in all supporting
+//      documentation.
+//
+//      AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+//      AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+//      MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+//      DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+//      UNINTERRUPTED OR ERROR FREE.
+//
+//      Use, duplication, or disclosure by the U.S. Government is subject to
+//      restrictions set forth in FAR 52.227-19 (Commercial Computer
+//      Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+//      (Rights in Technical Data and Computer Software), as applicable.
+//
+#endregion
+
+#region imports
+using System;
+using System.Windows;
+using System.Diagnostics;
+
+using UiViewModels.Actions;
+using Autodesk.Max;
+
+#endregion
+
+namespace ADNMenuSample
+{
+    public class MenuItemStrings
+    {
+        // just convienence for globals strings. Normally strings would probably be loaded from resources
+        public static string actionWindow = "3d Search";
+        public static string actionCategory = "3d Search";
+    }
+
+    public class RestructureModelWindow : CuiActionCommandAdapter
+    {
+        public bool isopen = false;
+        Window mainDialog = null;
+        public override void Execute(object parameter)
+        {
+            IGlobal global = Autodesk.Max.GlobalInterface.Instance;
+            IInterface14 ip = global.COREInterface14;
+            try
+            {
+                if (mainDialog == null)
+                {
+                    mainDialog = new ApplicationView();
+                }
+                if (mainDialog.Visibility == Visibility.Hidden)
+
+
+                    isopen = false;
+                if (!isopen)
+                {
+                    isopen = true;
+                    mainDialog.Closed += new EventHandler(SearchSimilar_Closed);
+                    System.Windows.Interop.WindowInteropHelper windowHandle = new System.Windows.Interop.WindowInteropHelper(mainDialog);
+                    windowHandle.Owner = ManagedServices.AppSDK.GetMaxHWND();
+                    ManagedServices.AppSDK.ConfigureWindowForMax(mainDialog);
+                    mainDialog.Show();
+                }
+                else
+                {
+                    isopen = false;
+                    mainDialog.Hide();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception occurred: " + e.Message);
+            }
+        }
+
+        void SearchSimilar_Closed(object sender, EventArgs e)
+        {
+            isopen = false;
+            mainDialog.Visibility = Visibility.Hidden;
+        }
+        public override string InternalActionText
+        {
+            get { return MenuItemStrings.actionWindow; }
+        }
+
+        public override string InternalCategory
+        {
+            get { return MenuItemStrings.actionCategory; }
+        }
+
+        public override string ActionText
+        {
+            get { return InternalActionText; }
+        }
+
+        public override string Category
+        {
+            get { return InternalCategory; }
+        }
+    }
+}
